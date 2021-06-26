48c8c9a80ebc9acf9cb7e5a8702faadd28190875
Victor Manuel Sarmiento Garcia <victorsarmiento8@gmail.com>
2021-06-26 18:24:58 -0500

    version1.03

--- data.R
+++ data.R
@@ -39,7 +39,7 @@ Label$Names <- names(d0506)
 
 Label <- Label[c(5, 1, 2, 3, 4)]
 
-write_xlsx(Label,"C:/Users/totos/Desktop/PDG/mochila/Label.xlsx")
+write_xlsx(Label,"C:/Users/totos/Desktop/PDG/mochila/Label0506.xlsx")
 
 a <- contents(d2007)
 
@@ -53,6 +53,48 @@ Label <- Label[c(5, 1, 2, 3, 4)]
 
 write_xlsx(Label,"C:/Users/totos/Desktop/PDG/mochila/Label07.xlsx")
 
+s <- full_join(d2005, d2006)
+
+d <- full_join(s, d2007)
+
+
+d2 <- full_join(d, d2008)
+
+d3 <- full_join(d2, d2009)
+
+d4 <- full_join(d3, d2010)
+
+
+
+
+a <- contents(d2009)
+
+  class(a)
+
+Label <- data.frame(a$contents)
+
+Label$Names <- names(d2009)
+
+Label <- Label[c(5, 1, 2, 3, 4)]
+
+write_xlsx(Label,"C:/Users/totos/Desktop/PDG/mochila/Label09.xlsx")
+
+
+val_lab(d2009$occu)
+summary(d2009$occu)
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 names(d2005)
@@ -60,7 +102,6 @@ names(d2006)
 summary(d2005)
 max(d2005$gender)
 
-contents(d0506)
 
 
 
